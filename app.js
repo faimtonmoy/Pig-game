0a2fa40874e9e5a91cff86353039683cb98a76a0
FaimTonmoy <59766142+faimtonmoy@users.noreply.github.com>
2020-06-05 22:45:38 +0600

    Update app.js

--- app.js
+++ app.js
@@ -74,5 +74,5 @@ document.querySelector('.player-0-panel').classList.remove('winner');
 document.querySelector('.player-1-panel').classList.remove('winner');
 document.querySelector('.player-0-panel').classList.remove('active');
 document.querySelector('.player-0-panel').classList.add('winner');
-document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('active');
 }
